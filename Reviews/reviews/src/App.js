8089e83f83f882dd4a49266329a8dbea1bd3543d
Gustavo Pereira <xgustavux@hotmail.com>
2021-12-16 21:27:37 +1000

    reviews project finalized

--- Reviews/reviews/src/App.js
+++ Reviews/reviews/src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import reviews from "./data";
+import { useState } from "react";
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from "react-icons/fa"
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const { id, name, job, image, text } = reviews[index];
+
+  const checkIndex = (num) => {
+    if (num < 0) {
+      return reviews.length - 1;
+    }
+
+    if (num > reviews.length - 1) {
+      return 0;
+    }
+    else {
+      return num;
+    }
+  }
+
+  const nextBtn = () => {
+    setIndex((index) => {
+      return  checkIndex(index + 1);
+    });
+  }
+
+   const prevBtn = () => {
+    setIndex((index) => {
+      return checkIndex(index - 1);
+    });
+  }
+
+  const randomBtn = () => {
+    const random = Math.round(Math.random() * (reviews.length - 1));
+    setIndex(random);
+    return checkIndex(random)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevBtn}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextBtn}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={randomBtn}>
+        surprise me
+      </button>
+    </article>  
   );
 }
 
 export default App;
+
+{/* <article className='review'>
+      <div className='img-container'>
+        <img src='' alt='' className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>name</h4>
+      <p className='job'>job</p>
+      <p className='info'>text</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' >
+        surprise me
+      </button>
+    </article> */}
