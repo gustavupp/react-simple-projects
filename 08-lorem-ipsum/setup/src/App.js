355f00c6c59ee9df6bb4688218689946ea244da7
Gustavo Pereira <xgustavux@hotmail.com>
2021-12-20 12:47:11 +1000

    done the lorem ipsum micro project

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [amount, setAmount] = useState(0);
+  const [arr, setArr] = useState([]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if(amount < 0 || amount > data.length){
+      console.log(amount)
+      setArr(data[Math.floor(Math.random() * data.length)]);
+    } else {
+      let temp = data.slice(0, amount);
+      let newTemp = temp.map((item, index) => {
+        return <p key={index}>{item}</p>;
+      })
+      setArr(newTemp);
+      setAmount(0);
+    }
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+      <h3>tired of boring lorem ipvcsum?</h3>
+      <form className='lorem-form'>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input type='number' id='amount' name='amount' value={amount} onChange={(e) => setAmount(e.target.value)}/>
+        <button className='btn' type='button' onClick={handleClick}>generate</button>
+      </form>
+      <article className='lorem-text'>{arr}
+      </article>
+    </section>
     )
 }
 
